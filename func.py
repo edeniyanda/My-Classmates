c8d7e0bd3e9c1bac6d5252fe9af3764d83f378ae
Eden Iyanda <edeniyanda@gmail.com>
2022-10-22 17:49:06 +0100

    A quick Update

--- func.py
+++ func.py
@@ -1,18 +1,24 @@
+from getpass import getpass
+
 data = {
     "eden":"password123",
     "edward":"pass123",
+    "quicksilver":"12345"
 }
-from getpass import getpass
-from webbrowser import get
+
 def start():
-    print("Welcome to the portal\nTo proceed please sign in")
-    welcome = input("Alredy have an account? ").capitalize()
-    if welcome == "Yes":
-        print("\n")
-        log_in()
-    elif welcome == "No":
-        print("\n")
-        signup()
+    print("Welcome to the portal\nTo proceed please sign in\n")
+    while True:
+        welcome = input("Alredy have an account? ").capitalize()
+        if welcome == "Yes":
+            print("\n")
+            log_in()
+        elif welcome == "No":
+            print("\n")
+            signup()
+        else:
+            print('Invalid input, please enter with a "Yes" or "No"\n')
+            
 def log_in():
     global data
     username = input("Enter your user name: ")
@@ -26,11 +32,11 @@ def log_in():
             print("Invalid password or username")
             print("\n")
             log_in()
-            quit()
     print("Name not found")
     print("\n")
     signup()
     quit()
+    
 def signup():
     t_a = input("Do you want to Sign up? ").capitalize()
     if t_a == "Yes":
@@ -39,38 +45,53 @@ def signup():
         re_sign_up()
         print("/n")
         main()
-    else:
+    elif t_a =="No":
         print("Have a nice day!!")
         quit()
+    else:
+        print("Invalid Input")
+        signup()
 
 def re_sign_up():
-    name = input('Enter your First name: ')
-    name = input('Enter your Last name: ')
+    fname = input('Enter your First name: ').capitalize()
+    lname = input('Enter your Last name: ').capitalize()
     user_name = input("Enter the username you intend on using: ")
     password = getpass("Enter your password: ")
     confirm_password = getpass("Confirm password: ")
     if password == confirm_password:
         data[user_name] = password
-        print(f"{user_name} Your account has been created")
-        print("You can now login into your account\n")
-        log_in()
+        print(f"Congratulations {fname} {lname} Your account has been created")
+        print("You now have access to the Portal\n")
+        main()
     else:
         print("The password you entered does not match\nPlease resign up")
         print("\n")
         re_sign_up()
+        
 def main():
     global fhand
-    set = input("Enter the year you graduated from Olivet: ")
-    if set == "2022":
-        set = "mbox.txt"
-    fhand = open("mbox.txt")
+    year = input("Enter the year you graduated from Olivet Baptist High School: ")
+    if year == "2022":
+        year = "mbox.txt"
+    else:
+        try:
+            year = int(year)
+            print(year)
+            print("Invalid input ")
+            main()
+        except:
+            print('Enter a numeric data!')
+            main()
+            
+    fhand = open(year)
     print("This prompt you to check up a classmate's info: ")
     name = input("Enter your classmate's name: ").capitalize()
+    
     for line in fhand:
         line = line.rstrip()
         if name in line:
             print(line)
-            t_a = input("Do you want to run the program again?\n(Enter (y) for y and (n) for No) ")
+            t_a = input("Do you want to run the program again?\n(Enter (y) for y and (n) for No) ").lower()
             if t_a == "y":
                 main()
             else:
---
+++ pp.py
@@ -0,0 +1,5 @@
+ss = str(input("FF"))
+if len(ss) == 10 or len(ss) < 3:
+    print("go")
+else:
+    print("stop")
